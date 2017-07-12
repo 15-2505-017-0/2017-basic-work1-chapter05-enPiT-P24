ab07d1c78349b32eb9e28e2c28a4a0c025d2cf5e
enPiT-P24 <cureseven@icloud.com>
2017-07-12 11:59:19 +0900

    first commit

--- MySize/app/src/main/java/com/example/u90684/mysize/MainActivity.java
+++ MySize/app/src/main/java/com/example/u90684/mysize/MainActivity.java
@@ -1,9 +1,35 @@
 package com.example.u90684.mysize;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static class MainDialogFragment extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("保存処理");
+            builder.setMessage("保存します");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //保存処理
+                }
+            });
+            return builder.create();
+        }
+        public void onSaveButtonTapped(View view){
+            DialogFragment newFragment = new MainDialogFragment();
+            newFragment.show(getFragmentManager(),"MainFragmentDialog");
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
